a6a8b935c06a8650cf85c1eb50d202c1898de527
Ofek <ofekaz24@gmail.com>
2016-03-25 13:12:15 +0200

    Adding layouts

--- OnlineQuizChcker/gen/com/example/onlinequizproject/onlinequizproject/R.java
+++ OnlineQuizChcker/gen/com/example/onlinequizproject/onlinequizproject/R.java
@@ -13,8 +13,35 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int LinearLayout=0x7f060000;
+        public static final int MenuBar=0x7f060001;
+        public static final int connectionBtn=0x7f060006;
+        public static final int courseCodeText=0x7f06000e;
+        public static final int courseIdLbl=0x7f060004;
+        public static final int courseIdSpinner=0x7f060005;
+        public static final int editText1=0x7f060012;
+        public static final int lecturerBtn=0x7f06000a;
+        public static final int loginBtn=0x7f060011;
+        public static final int loginLbl=0x7f06000c;
+        public static final int pincodeText=0x7f060002;
+        public static final int studentBtn=0x7f060009;
+        public static final int studentListView=0x7f060007;
+        public static final int studentRegLbl=0x7f060003;
+        public static final int textView1=0x7f06000b;
+        public static final int textView2=0x7f060008;
+        public static final int textView3=0x7f06000d;
+        public static final int textView4=0x7f06000f;
+        public static final int textView5=0x7f060010;
+    }
+    public static final class layout {
+        public static final int lect_dropboxauthview=0x7f030000;
+        public static final int lect_studentregistrationview=0x7f030001;
+        public static final int main_view=0x7f030002;
+        public static final int stud_loginview=0x7f030003;
+    }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f040000;
     }
     public static final class style {
         /** 
@@ -38,10 +65,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f050001;
     }
 }
